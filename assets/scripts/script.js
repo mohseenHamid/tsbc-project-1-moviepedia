ddbbde2cce99778ee12b33215781b34b4ac4d452
mohseenHamid <mhsn@hotmail.co.uk>
2023-02-09 19:28:46 +0000

    add celebNinja function

---
+++ assets/scripts/script.js
@@ -0,0 +1,20 @@
+// FUNCTION FOR CELEBNINJA API
+function celebNinjaClosure(celebName) {
+	celebName = "michael jackson"; // PLACEHOLDER
+
+	function celebNinjaInner() {
+		$.ajax({
+			method: "GET",
+			url: "https://api.celebrityninjas.com/v1/search?name=" + celebName,
+			headers: { "X-Api-Key": "+qZ8SEACFRjRVK2XZ9RpgQ==urpaH1jT1cOiYaJ6" },
+			contentType: "application/json"
+		}).then((result) => {
+			console.log(result);
+			let;
+		});
+	}
+	return celebNinjaInner;
+}
+let celebNinja = celebNinjaClosure();
+
+celebNinja();
