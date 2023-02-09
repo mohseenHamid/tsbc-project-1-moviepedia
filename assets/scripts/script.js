afd919823deb51ab8bc20f5e494ccfa9949597e4
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2023-02-09 19:29:08 +0000

    functions to return search results into div and function to pull movie data

---
+++ assets/scripts/script.js
@@ -0,0 +1,44 @@
+const OMDB_API_KEY = "e2bf0e18";
+
+// function to pull data from omdb API for searching
+function searchMovieTitles(searchString){
+  const queryURL = "http://www.omdbapi.com/?apikey=" + OMDB_API_KEY + "&s=" + searchString + "*";
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(response){
+    const resultsArray = response.Search;
+    const resultsDiv = $('#searchResults');
+    resultsDiv.empty();
+    resultsArray.forEach(element => {
+      console.log(element.Title + " (" + element.Year + ")");
+      const movieTitle = $('<div>').text(element.Title + " (" + element.Year + ")");
+      resultsDiv.append(movieTitle)
+    });
+  })
+}
+
+// function to pull data from omdb API for data of selected movie
+function getMovieDetails(movieTitle){
+  const queryURL = "http://www.omdbapi.com/?apikey=" + OMDB_API_KEY + "&t=" + movieTitle;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(response){
+    console.log(response);
+  })
+}
+
+// function to add keyup handler once document is ready
+$(function(){
+  $('#movieName').on('keyup', function(event){
+    // when key is released this function runs grabbing the text that is in the input box
+    const textString = $('#movieName').val();
+    // first it checks if more than 2 characters have been typed as 2 or less gives a too many results
+    // error from the API
+    if(textString.length>2){
+      // if more than two characters then it calls the searchMovieTitles function
+      searchMovieTitles(textString);
+    }
+  })
+})
