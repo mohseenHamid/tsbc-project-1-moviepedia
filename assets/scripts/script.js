88beb8af1390594d084c0fd8427caaba8b1f879f
mohseenHamid <mhsn@hotmail.co.uk>
2023-02-14 18:25:51 +0000

    search modal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,25 @@
 const omdbKey = "e2bf0e18";
 
-// Poster test
-const testURL = "http://www.omdbapi.com/?apikey=" + omdbKey + "&s=titanic*";
-$.ajax({
-	url: testURL,
-	method: "GET"
-}).then(function (response) {
-	console.log(response.Poster);
-	console.log(response.Title);
-});
+// FUNCTION FOR CELEBNINJA API
+function celebNinjaClosure(celebName) {
+	celebName = "michael jackson"; // PLACEHOLDER
+
+	function celebNinjaInner() {
+		$.ajax({
+			method: "GET",
+			url: "https://api.celebrityninjas.com/v1/search?name=" + celebName,
+			headers: { "X-Api-Key": "+qZ8SEACFRjRVK2XZ9RpgQ==urpaH1jT1cOiYaJ6" },
+			contentType: "application/json"
+		}).then((result) => {
+			// console.log(result[0].name);
+			// console.log(result[0].birthday);
+			// console.log(result[0].net_worth);
+			// console.log(result[0].height);
+		});
+	}
+	return celebNinjaInner;
+}
+let celebNinja = celebNinjaClosure();
 
 // function to pull data from omdb API for searching
 function searchMovieTitles(searchString) {
@@ -50,9 +61,26 @@ function searchMovieTitles(searchString) {
 	});
 }
 
+// function to find actor details from wikipedia
+function actorSearch(actor) {
+	$.ajax({
+		method: "GET",
+		url: "https://en.wikipedia.org/api/rest_v1/page/summary/" + actor,
+		contentType: "application/json"
+	}).then(function (result) {
+		// const actorProfile = {
+		// 	actorName: result.title,
+		// 	thumbnail: result.thumbnail.source,
+		// 	bio: result.extract
+		// };
+	});
+}
+
+console.log(actorSearch("tom_hanks"));
+
 // function to pull data from omdb API for data of selected movie
 function getMovieDetails(movieTitle, movieYear) {
-	const queryURL =
+	const movieURL =
 		"http://www.omdbapi.com/?apikey=" +
 		omdbKey +
 		"&plot=full&t=" +
@@ -60,72 +88,117 @@ function getMovieDetails(movieTitle, movieYear) {
 		"&y=" +
 		movieYear;
 	$.ajax({
-		url: queryURL,
+		url: movieURL,
 		method: "GET"
 	}).then(function (response) {
-		console.log(response);
-		console.log(response.Poster);
-		console.log(response.Title);
-		console.log(response.Year);
-		console.log(response.Rated);
-		console.log(response.Runtime);
-		console.log(response.Plot);
-		response.Ratings.forEach(function (rating) {
-			console.log(rating.Source);
-			console.log(rating.Value);
-		});
+		let posterURL = response.Poster;
+		let movieTitle = response.Title;
+		let movieYear = response.Year;
+		let runtime = response.Runtime;
+		let moviePlot = response.Plot;
+		let movieRatedTag = response.Rated;
+		let movieRatings = response.Ratings;
 		let actors = response.Actors.split(",");
-		console.log(actors);
+
+		// Set movie modal row 1 properties/values
+		$("#modal-movie-poster").attr("src", posterURL);
+		$(".c2-r1-c1").text(`${movieTitle} (${movieYear})`);
+		$(".c2-r1-c2").text(`Rated: ${movieRatedTag}`);
+		$(".c2-r1-c3").text(`Runtime: ${runtime}`);
+		$(".plot-content").text(`${moviePlot}`);
+
+		let ratingsArrayLength = movieRatings.length;
+		for (let i = 1; i < ratingsArrayLength + 1; i++) {
+			// Extracting column label
+			let column = `.c2-r4-c${i}`;
+
+			// assign column text
+			$(column).text(
+				`${movieRatings[i - 1].Source}: ${movieRatings[i - 1].Value}`
+			);
+		}
+
+		// Set movie modal row 2 properties/values using Wiki API
+		actors.forEach((actor) => {
+			console.log(actorSearch(actor));
+		});
 	});
+	// Display modal
+	$("#exampleModalCenter").modal("show");
 }
 // function to get movieDetails from data attributes and load search-result with those details
 //  as parameters
 function goToSearchResult(event) {
+	$(".movie-input").val("");
+	$("#search-menu").css("display", "none");
+
 	const movieTitle = event.target.getAttribute("data-movie-title");
 	const movieYear = event.target.getAttribute("data-movie-year");
-	window.location.href =
-		"./search-result.html?title=" + movieTitle + "&year=" + movieYear;
-	// console.log(movieTitle, movieYear);
+
+	// OMDB API to get the modal row 1 details
+	getMovieDetails(movieTitle, movieYear);
+}
+// function goToSearchResult(event) {
+// 	const movieTitle = event.target.getAttribute("data-movie-title");
+// 	const movieYear = event.target.getAttribute("data-movie-year");
+// 	window.location.href =
+// 		"./search-result.html?title=" + movieTitle + "&year=" + movieYear;
+// 	// console.log(movieTitle, movieYear);
+// 	getMovieDetails(movieTitle, movieYear);
+// }
+
+function returnSearchResults(event) {
+	// when key is released this function runs grabbing the text that is in the input box
+	const textString = $(".movie-input").val();
+	// Sets dropdown display default as "none" to solve empty dropdown display bug while API is being called
+	$("#search-menu").css("display", "none");
+
+	// first it checks if more than 2 characters have been typed as 2 or less gives too many results
+	// error from the API
+	if (textString.length > 2 && event.keyCode !== 27) {
+		// if more than two characters then it calls the searchMovieTitles function
+		searchMovieTitles(textString);
+
+		// Solve bug: code runs before API call is completed
+		setTimeout(() => {
+			if ($("#search-menu").children().length == 0) {
+				$("#search-menu").css("display", "none");
+			} else {
+				$("#search-menu").css("display", "block");
+			}
+		}, 500);
+	} else if (textString.length < 3) {
+		$("#search-menu").empty();
+	} else if (event.keyCode == 27) {
+		$(this).val("");
+		$("#search-menu").empty();
+	}
 }
 
-// function to add keyup handler once document is ready
+// Saving movie to favourites carousel
+function saveMovie(e) {
+	console.log(e.parent());
+}
+
+// Document Ready Event Handlers
 $(function () {
-	$(".movie-input").on("keyup", function (event) {
-		// when key is released this function runs grabbing the text that is in the input box
-		const textString = $(".movie-input").val();
-		// first it checks if more than 2 characters have been typed as 2 or less gives too many results
-		// error from the API
+	// Keyup event listener for movie search input field
+	$(".movie-input").on("keyup", returnSearchResults);
 
-		// Sets dropdown display default as "none" to solve empty dropdown display bug while API is being called
-		$("#search-menu").css("display", "none");
-
-		if (textString.length > 2) {
-			// if more than two characters then it calls the searchMovieTitles function
-			searchMovieTitles(textString);
-
-			// Solve bug: code runs before API call is completed
-			setTimeout(() => {
-				if ($("#search-menu").children().length == 0) {
-					$("#search-menu").css("display", "none");
-				} else {
-					console.log("DROPDOWN TRIGGERED");
-					console.log(textString);
-					$("#search-menu").css("display", "block");
-				}
-			}, 500);
-		} else {
-			$("#search-menu").empty();
-		}
-
-		// Emptying field with Escape key
-		if (event.keyCode == 27) {
-			$(this).val("");
-			$("#search-menu").empty();
-			$("#search-menu").css("display", "none");
-		}
-	});
-
+	// Click event listener for movie search menu dropdown item selection
 	$("#search-menu").on("click", goToSearchResult);
+
+	// Bug: dropdown menu disappears when selecting search menu items
+	// $(".movie-input").on("click", returnSearchResults);
+	// $(".movie-input").focusout((e) => {
+	// 	if (e.target !== $("#search-menu")) {
+	// 		$("#search-menu").css("display", "none");
+	// 	} else {
+	// 		$("#search-menu").css("display", "block");
+	// 	}
+	// });
+
+	// WHAT IS THIS? Is this to set the new page URL?
 	if (window.location.search !== "") {
 		const queryString = window.location.search;
 		const urlParams = new URLSearchParams(queryString);
@@ -133,43 +206,9 @@ $(function () {
 		const movieYear = urlParams.get("year");
 		getMovieDetails(movieTitle, movieYear);
 	}
+
+	$("#movie-fav-save-btn").on("click", saveMovie);
 });
-
-// FUNCTION FOR CELEBNINJA API
-function celebNinjaClosure(celebName) {
-	celebName = "michael jackson"; // PLACEHOLDER
-
-	function celebNinjaInner() {
-		$.ajax({
-			method: "GET",
-			url: "https://api.celebrityninjas.com/v1/search?name=" + celebName,
-			headers: { "X-Api-Key": "+qZ8SEACFRjRVK2XZ9RpgQ==urpaH1jT1cOiYaJ6" },
-			contentType: "application/json"
-		}).then((result) => {
-			console.log(result[0].name);
-			console.log(result[0].birthday);
-			console.log(result[0].net_worth);
-			console.log(result[0].height);
-		});
-	}
-	return celebNinjaInner;
-}
-let celebNinja = celebNinjaClosure();
-
-// function to find actor details from wikipedia
-function actorSearch(actorName) {
-	$.ajax({
-		method: "GET",
-		url: "https://en.wikipedia.org/api/rest_v1/page/summary/" + actorName,
-		contentType: "application/json"
-	}).then(function (result) {
-		console.log(result.title);
-		console.log(result.thumbnail.source);
-		console.log(result.extract);
-	});
-}
-
-celebNinja();
 
 //  not sure we need this document.ready function apart from for testing
 // $(document).ready(function() {
